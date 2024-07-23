6578d71045d0de576f14e385caa9b54670547df2
Tori-Satsumi <truongthaitan2802@gmail.com>
2024-07-23 14:51:03 +0000

    Jul 23, 2024, 9:51 PM

--- flappy/sprite_data.py
+++ flappy/sprite_data.py
@@ -12,14 +12,14 @@ MEDAL_RAD = 3 * SQUARE_PIXEL
  
 sprite_data : dict = {
     "bg_day"         : (0, 0, 144, 256),
-    "bg_night"       : (144 + GAP, 0, 144, 256),
-    "ground"         : (144 * 2 + GAP * 2, 0, 144, 7 * SQUARE_PIXEL),
-    "score_board"    : (0, 256, 15 * SQUARE_PIXEL, 8 * SQUARE_PIXEL),
-    "bronze medal"   : (14 * SQUARE_PIXEL - 1, 19 * SQUARE_PIXEL + 320 + 4, MEDAL_RAD, MEDAL_RAD),
-    "silver_medal"   : (14 * SQUARE_PIXEL - 1, 16 * SQUARE_PIXEL + 320 + 4, MEDAL_RAD, MEDAL_RAD),
-    "gold_medal"     : (15 * SQUARE_PIXEL, 256 + MEDAL_RAD + 1, MEDAL_RAD, MEDAL_RAD),
-    "platinum medal" : (15 * SQUARE_PIXEL, 256 + 1, MEDAL_RAD, MEDAL_RAD),
-    "blue_bird_1"    : (14 * SQUARE_PIXEL + GAP, 320, 17, 12)
+    "bg_night"       : (146, 0, 144, 256),
+    "ground"         : (144 * 2 + GAP * 2, 0, 144, 56),
+    "score_board"    : (0, 256, 120, 8 * SQUARE_PIXEL),
+    "bronze medal"   : (111, 19 * SQUARE_PIXEL + 320 + 4, MEDAL_RAD, MEDAL_RAD),
+    "silver_medal"   : (111, 16 * SQUARE_PIXEL + 320 + 4, MEDAL_RAD, MEDAL_RAD),
+    "gold_medal"     : (120, 256 + MEDAL_RAD + 1, MEDAL_RAD, MEDAL_RAD),
+    "platinum medal" : (120, 256 + 1, MEDAL_RAD, MEDAL_RAD),
+    "blue_bird_1"    : (114, 320, 17, 12)
 
 
 }
