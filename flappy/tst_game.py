e26cfdba982095eff6aef15e0c116ef6c508a5aa
Tori-Satsumi <truongthaitan2802@gmail.com>
2024-07-23 10:39:18 +0000

    Jul 23, 2024, 5:39 PM

---
+++ flappy/tst_game.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+screen.fill("BLack")
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+            
+    screen.fill("BLack")    
+    
+    
+    pygame.display.update()
+    clock.tick(60)
