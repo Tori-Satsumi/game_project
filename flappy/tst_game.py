66d5cc1ef90296551703981250210bc61412a84e
Tori-Satsumi <truongthaitan2802@gmail.com>
2024-07-23 14:38:12 +0000

    Jul 23, 2024, 9:38 PM

--- flappy/tst_game.py
+++ flappy/tst_game.py
@@ -1,10 +1,29 @@
 from sprite_data import *
 import pygame
 
+def check_file_exist(file_name : str = "") -> str:
+    # find the file path in the user desktop
+    if file_name:
+        for root, dirs, files in os.walk("C:\\Users\\GIGABYTE\\OneDrive\\Desktop"):
+            for name in files:
+                if name == file_name:
+                    return os.path.abspath(os.path.join(root, name))
+            for directory in dirs:
+                if directory == file_name:
+                    return os.path.abspath(os.path.join(root, directory))                    
+    return ""
+
 pygame.init()
+
+if not (sprite := check_file_exist("flappy_bird_sprite.png")):
+    pygame.quit()
+    exit()
+
 
 screen = pygame.display.set_mode((500, 500))
 screen.fill("BLack")
+
+
 
 clock = pygame.time.Clock()
 
