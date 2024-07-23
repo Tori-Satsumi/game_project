1b7e7e362492b80a11faf2a135394840f96d3a18
Tori-Satsumi <truongthaitan2802@gmail.com>
2024-07-23 14:47:58 +0000

    Jul 23, 2024, 9:47 PM

--- flappy/sprite_data.py
+++ flappy/sprite_data.py
@@ -15,11 +15,11 @@ sprite_data : dict = {
     "bg_night"       : (SCREEN_WIDTH + GAP, 0, SCREEN_WIDTH, SCREEN_HEIGHT),
     "ground"         : (SCREEN_WIDTH * 2 + GAP * 2, 0, SCREEN_WIDTH, 7 * SQUARE_PIXEL),
     "score_board"    : (0, SCREEN_HEIGHT, 15 * SQUARE_PIXEL, 8 * SQUARE_PIXEL),
-    "bronze medal"   : (14 * SQUARE_PIXEL - 1, 19 * SQUARE_PIXEL + SCREEN_HEIGHT + 8 * SQUARE_PIXEL + 4, MEDAL_RAD, MEDAL_RAD),
-    "silver_medal"   : (14 * SQUARE_PIXEL - 1, 16 * SQUARE_PIXEL + SCREEN_HEIGHT + 8 * SQUARE_PIXEL + 4, MEDAL_RAD, MEDAL_RAD),
+    "bronze medal"   : (14 * SQUARE_PIXEL - 1, 19 * SQUARE_PIXEL + 320 + 4, MEDAL_RAD, MEDAL_RAD),
+    "silver_medal"   : (14 * SQUARE_PIXEL - 1, 16 * SQUARE_PIXEL + 320 + 4, MEDAL_RAD, MEDAL_RAD),
     "gold_medal"     : (15 * SQUARE_PIXEL, SCREEN_HEIGHT + MEDAL_RAD + 1, MEDAL_RAD, MEDAL_RAD),
     "platinum medal" : (15 * SQUARE_PIXEL, SCREEN_HEIGHT + 1, MEDAL_RAD, MEDAL_RAD),
-    "blue_bird_1"    : (14 * SQUARE_PIXEL + GAP, SCREEN_HEIGHT + 8 * SQUARE_PIXEL, 17, 12)
+    "blue_bird_1"    : (14 * SQUARE_PIXEL + GAP, 320, 17, 12)
 
 
 }
