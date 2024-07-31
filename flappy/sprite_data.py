1dec33cd972b1be96930944efe44680606b8cd95
Tori-Satsumi <truongthaitan2802@gmail.com>
2024-07-31 14:46:16 +0000

    Jul 31, 2024, 9:46 PM

--- flappy/sprite_data.py
+++ flappy/sprite_data.py
@@ -22,7 +22,7 @@ sprite_data : dict = {
     "blue_bird_1"    : (114, 328, 20, 14),
     "blue_bird_2"    : (114, 354, 20, 14),
     "blue_bird_3"    : (86, 490, 20, 14),
-    "yellow_bird_1"  : ()
+    "yellow_bird_1"  : (2, 490, 20, 14)
 
 
 }
