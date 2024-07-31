ffceb6099dc77257fb0303b72c82334bd1ff91dc
Tori-Satsumi <truongthaitan2802@gmail.com>
2024-07-31 12:04:21 +0000

    Jul 31, 2024, 7:04 PM

--- flappy/tst.py
+++ flappy/tst.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-with Image.open('C:\Users\GIGABYTE\OneDrive\Desktop\program\game_project\flappy\img\flappy_bird_sprite.png') as img:
+with Image.open('C:/Users/GIGABYTE/OneDrive/Desktop/program/game_project/flappy/img/flappy_bird_sprite.png') as img:
     width, height = img.size
 
     print(width)
