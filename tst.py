499cb45e845357bd273ff3e25c99a52507c7f1d8
Tori-Satsumi <truongthaitan2802@gmail.com>
2024-08-04 13:26:59 +0000

    Aug 4, 2024, 8:26 PM

--- tst.py
+++ tst.py
@@ -1,7 +1,18 @@
-SCREEN_DISPLAY = {
-    "width" : 800,
-    "height" : 800
-}
-SCREEN_DISPLAY = tuple(SCREEN_DISPLAY.values())
 
-print(SCREEN_DISPLAY)
+
+while True:
+    nums = input("number: ")
+    try:
+        nums = float(int(nums))
+        break
+    except:
+        pass
+    
+total = 0
+for i, num in enumerate(str(nums), 1):
+    total += num ** i
+    
+if total == nums:
+    print("Disarium number")
+else:
+    print("not Disarium number")
